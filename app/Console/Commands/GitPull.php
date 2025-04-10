8b164866558551bf95ca41fb759ecd531988bad8
Sanusi15 <websan1512@gmail.com>
2025-04-10 11:47:33 +0700

    update git pull di command

--- app/Console/Commands/GitPull.php
+++ app/Console/Commands/GitPull.php
@@ -12,10 +12,12 @@ class GitPull extends Command
 
     public function handle()
     {
-        $projectPath = base_path(); // atau ganti path-nya jika di luar Laravel
-        $output = [];
+        // $projectPath = base_path(); // atau ganti path-nya jika di luar Laravel
+        // exec("cd {$projectPath} && git pull origin main 2>&1", $output);
+        // $output = [];
 
-        exec("cd {$projectPath} && git pull origin main 2>&1", $output);
+        $command = 'export HOME=/home/digik3882 && cd /home/digikom.xyz/dev && git pull origin main 2>&1';
+        $output = shell_exec($command);
 
         Log::info('Git Pull Output:', $output);
         $this->info(implode("\n", $output));
