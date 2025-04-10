7ad863cad8177eafc087fe27055850fc360ea414
Sanusi15 <websan1512@gmail.com>
2025-04-10 16:15:23 +0700

    update log pada callbackEwallet

--- app/Http/Controllers/Api/CallbackPayment/CallbackEwalletController.php
+++ app/Http/Controllers/Api/CallbackPayment/CallbackEwalletController.php
@@ -11,7 +11,7 @@ class CallbackEwalletController extends Controller
     public function handle(Request $request)
     {
         // Simpan log callback untuk debug awal
-        Log::info('Xendit Callback:', $request->all());
+        Log::channel('single')->info('Xendit Callback:', $request->all());
 
         $token = $request->header('x-callback-token');
         $callBackToken = env('XENDIT_CALLBACK_TOKEN');
