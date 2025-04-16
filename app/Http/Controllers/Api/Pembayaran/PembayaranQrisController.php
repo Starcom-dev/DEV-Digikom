8d0a46dad74f269c66f8d285422057db920727ec
Sanusi15 <websan1512@gmail.com>
2025-04-16 09:15:42 +0700

    perbaikan callback url pada payload transaction qris

--- app/Http/Controllers/Api/Pembayaran/PembayaranQrisController.php
+++ app/Http/Controllers/Api/Pembayaran/PembayaranQrisController.php
@@ -49,7 +49,7 @@ class PembayaranQrisController extends Controller
                 'type' => 'DYNAMIC',
                 'currency' => 'IDR',
                 'amount' => $nominal,
-                'callback_url' => config('app.url') . 'api/callbackVa',
+                'callback_url' => config('app.url') . 'api/callbackQris',
             ];
 
             Log::channel('single')->debug('Payload untuk API Xendit QRIS', $payload);
