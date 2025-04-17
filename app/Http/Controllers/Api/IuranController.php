897eea2ad5bf6d6be824e20f3bed06c5bfe3e5e7
Sanusi15 <websan1512@gmail.com>
2025-04-17 16:51:15 +0700

    pemindahan cek have transaction dari middleware CheckMembership ke IuranController

--- app/Http/Controllers/Api/IuranController.php
+++ app/Http/Controllers/Api/IuranController.php
@@ -7,16 +7,39 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\IuranResource;
 use App\Models\Iuran;
+use App\Models\Tagihan;
+use Illuminate\Support\Facades\Log;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class IuranController extends Controller
 {
     public function index()
     {
-        //get all posts
-        // $iurans = Iuran::latest()->paginate(5);
-        $iurans = Iuran::orderBy('harga', 'ASC')->get();
-        //return collection of posts as a resource
-        return new IuranResource(true, 'List Data Subscription', $iurans);
+        $user = JWTAuth::parseToken()->authenticate();
+        $getHistoryTagihan = Tagihan::with(['iuran', 'transactions', 'opsiBayar'])->where(['user_id' => $user->id, 'status' => 'Belum Lunas'])->orderBy('created_at', 'DESC')->first();
+        Log::channel('single')->info('Get History Tagihan' . $getHistoryTagihan);
+        if ($getHistoryTagihan) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Anda sudah melakukan transaksi, silahkan melakukan pembayaran',
+                'data' => [
+                    'subscription' => [
+                        'harga' => $getHistoryTagihan->iuran->harga,
+                        'keterangan' => $getHistoryTagihan->iuran->keterangan,
+                    ],
+                    'detail_pembayaran' => [
+                        'id_transaksi' => $getHistoryTagihan->transactions->id_transaction,
+                        'metode_pembayaran' => $getHistoryTagihan->opsiBayar->opsi_bayar,
+                        'kode_bayar' => $getHistoryTagihan->kode_bayar
+                    ]
+                ]
+            ], 403);
+        } else {
+            //get all posts
+            $iurans = Iuran::orderBy('harga', 'ASC')->get();
+            //return collection of posts as a resource
+            return new IuranResource(true, 'List Data Subscription', $iurans);
+        }
     }
 
     public function show($id)
--- app/Http/Middleware/CheckMembership.php
+++ app/Http/Middleware/CheckMembership.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Tagihan;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -20,26 +18,6 @@ class CheckMembership
         $user = JWTAuth::parseToken()->authenticate();
 
         if ($user->is_membership !== 'true') {
-            $getHistoryTagihan = Tagihan::with(['iuran', 'transactions', 'opsiBayar'])->where(['user_id' => $user->id, 'status' => 'Belum Lunas'])->orderBy('created_at', 'DESC')->first();
-            Log::channel('single')->info('Get History Tagihan' . $getHistoryTagihan);
-            if ($getHistoryTagihan) {
-                return response()->json([
-                    'success' => false,
-                    'is_checkout' => true,
-                    'message' => 'Anda sudah melakukan transaksi, silahkan melakukan pembayaran',
-                    'data' => [
-                        'subscription' => [
-                            'harga' => $getHistoryTagihan->iuran->harga,
-                            'keterangan' => $getHistoryTagihan->iuran->keterangan,
-                        ],
-                        'detail_pembayaran' => [
-                            'id_transaksi' => $getHistoryTagihan->transactions->id_transaction,
-                            'metode_pembayaran' => $getHistoryTagihan->opsiBayar->opsi_bayar,
-                            'kode_bayar' => $getHistoryTagihan->kode_bayar
-                        ]
-                    ]
-                ], 403);
-            }
             return response()->json([
                 'success' => false,
                 'is_checkout' => false,
