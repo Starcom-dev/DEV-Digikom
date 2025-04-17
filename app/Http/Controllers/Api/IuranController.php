e98344b6e4938ee47e432d5d478bddafd0a848a3
Sanusi15 <websan1512@gmail.com>
2025-04-17 17:16:29 +0700

    update response ketika get data subscription namun user sudah create transaksi

--- app/Http/Controllers/Api/IuranController.php
+++ app/Http/Controllers/Api/IuranController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\IuranResource;
 use App\Models\Iuran;
 use App\Models\Tagihan;
@@ -19,20 +17,21 @@ class IuranController extends Controller
         $getHistoryTagihan = Tagihan::with(['iuran', 'transactions', 'opsiBayar'])->where(['user_id' => $user->id, 'status' => 'Belum Lunas'])->orderBy('created_at', 'DESC')->first();
         Log::channel('single')->info('Get History Tagihan' . $getHistoryTagihan);
         if ($getHistoryTagihan) {
+            $nominal = $getHistoryTagihan->iuran->nominal;
+            $adminFee = $getHistoryTagihan->opsiBayar->biaya_tetap;
             return response()->json([
                 'success' => true,
-                'message' => 'Anda sudah melakukan transaksi, silahkan melakukan pembayaran',
+                'message' => 'Transaksi telah dibuat, silahkan melakukan pembayaran.',
+                'nominal' => $nominal,
+                'admin' => $adminFee,
+                'total' => $nominal + $adminFee,
+                'id_transaksi' => $getHistoryTagihan->transactions->id_transaction,
                 'data' => [
-                    'subscription' => [
-                        'harga' => $getHistoryTagihan->iuran->harga,
-                        'keterangan' => $getHistoryTagihan->iuran->keterangan,
-                    ],
-                    'detail_pembayaran' => [
-                        'id_transaksi' => $getHistoryTagihan->transactions->id_transaction,
-                        'metode_pembayaran' => $getHistoryTagihan->opsiBayar->opsi_bayar,
-                        'kode_bayar' => $getHistoryTagihan->kode_bayar
-                    ]
-                ]
+                    'id' => '',
+                    'status' => $getHistoryTagihan->status,
+                    'channel_code' => $getHistoryTagihan->opsiBayar->opsi_bayar,
+                    'kode_bayar' => $getHistoryTagihan->kode_bayar, // Ambil account_number dari response Xendit
+                ],
             ], 403);
         } else {
             //get all posts
