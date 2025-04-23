7220052b5e74612514ce5837f7d4d73cb58edcc3
Sanusi15 <websan1512@gmail.com>
2025-04-23 13:30:52 +0700

    penambahan validasi cek amount pada callback va

--- app/Http/Controllers/Api/CallbackPayment/CallbackVaController.php
+++ app/Http/Controllers/Api/CallbackPayment/CallbackVaController.php
@@ -28,47 +28,50 @@ class CallbackVaController extends Controller
             $payload = json_decode(json_encode($request->all()));
             // $status = $payload->data->status ?? null;
             $external_id = $payload->external_id ?? null;
-            $transaction = DB::table('transactions')->where('id_transaction', $external_id)->first();
-            if ($transaction) {
-                // update status transasction pada table transactions
-                DB::table('transactions')->where([
-                    'id_transaction' => $transaction->id_transaction,
-                    'tagihan_id' => $transaction->tagihan_id
-                ])->update(['status_transaction' => 'success']);
-                // get data tagihan
-                $tagihan = DB::table('tagihans')->where(['id' => $transaction->tagihan_id])->first();
-                // update status, tgl bayar dan payment_status pada table tagihans
-                DB::table('tagihans')->where([
-                    'id' => $transaction->tagihan_id
-                ])->update([
-                    'tanggal_bayar' => now(),
-                    'status' => 'Lunas',
-                    'payment_status' => 'SUCCEEDED',
-                    'updated_at' => now()
-                ]);
-                // get detail iuran membership
-                $iuran = DB::table('iurans')->where([
-                    'id' => $tagihan->iuran_id,
-                ])->first();
-                // update status membership user
-                $typeMembership = $iuran->masa_aktif;
-                $membershipStart = Carbon::now();
-                $membershipEnd = Carbon::now()->addMonths($typeMembership);
-                DB::table('users')->where([
-                    'id' => $transaction->user_id
-                ])->update([
-                    'is_membership' => 'true',
-                    'membership_start' => $membershipStart,
-                    'membership_end' => $membershipEnd
-                ]);
+
+            if (isset($payload->amount)) {
+                $transaction = DB::table('transactions')->where('id_transaction', $external_id)->first();
+                if ($transaction) {
+                    // update status transasction pada table transactions
+                    DB::table('transactions')->where([
+                        'id_transaction' => $transaction->id_transaction,
+                        'tagihan_id' => $transaction->tagihan_id
+                    ])->update(['status_transaction' => 'success']);
+                    // get data tagihan
+                    $tagihan = DB::table('tagihans')->where(['id' => $transaction->tagihan_id])->first();
+                    // update status, tgl bayar dan payment_status pada table tagihans
+                    DB::table('tagihans')->where([
+                        'id' => $transaction->tagihan_id
+                    ])->update([
+                        'tanggal_bayar' => now(),
+                        'status' => 'Lunas',
+                        'payment_status' => 'SUCCEEDED',
+                        'updated_at' => now()
+                    ]);
+                    // get detail iuran membership
+                    $iuran = DB::table('iurans')->where([
+                        'id' => $tagihan->iuran_id,
+                    ])->first();
+                    // update status membership user
+                    $typeMembership = $iuran->masa_aktif;
+                    $membershipStart = Carbon::now();
+                    $membershipEnd = Carbon::now()->addMonths($typeMembership);
+                    DB::table('users')->where([
+                        'id' => $transaction->user_id
+                    ])->update([
+                        'is_membership' => 'true',
+                        'membership_start' => $membershipStart,
+                        'membership_end' => $membershipEnd
+                    ]);
+                    DB::commit();
+                    Log::channel('single')->info('Update status transaction berhasil');
+                } else {
+                    DB::rollBack();
+                    Log::warning("Transaksi dengan ID $external_id tidak ditemukan.");
+                }
                 DB::commit();
-                Log::channel('single')->info('Update status transaction berhasil');
-            } else {
-                DB::rollBack();
-                Log::warning("Transaksi dengan ID $external_id tidak ditemukan.");
+                return response('', 200);
             }
-            DB::commit();
-            return response('', 200);
         } catch (\Throwable $th) {
             Log::channel('single')->error('Xendit Error Callback Va ' . $th);
             DB::rollBack();
