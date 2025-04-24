35673c41e55318f51799a6735953076aee59fdea
Sanusi15 <websan1512@gmail.com>
2025-04-24 10:16:08 +0700

    tes

--- app/Models/BidangUsaha.php
+++ app/Models/BidangUsaha.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class BidangUsaha extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    public $timestamps = true;
     /**
      * fillable
      *
--- app/Models/CategoryUsaha.php
+++ app/Models/CategoryUsaha.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryUsaha extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    public $timestamps = true;
     /**
      * fillable
      *
