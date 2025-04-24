8a29a1d5c8f3b84bb921639e10a9789335e2ef58
Sanusi15 <websan1512@gmail.com>
2025-04-24 10:08:56 +0700

    create store function pada bidang usaha

--- app/Http/Controllers/Api/BidangUsahaController.php
+++ app/Http/Controllers/Api/BidangUsahaController.php
@@ -14,4 +14,24 @@ class BidangUsahaController extends Controller
         $bidangUsaha = BidangUsaha::orderBy('nama', 'ASC')->get();
         return new BidangUsahaResource(true, 'List Data Bidang Usaha', $bidangUsaha);
     }
+
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'nama' => 'required'
+            ]);
+            BidangUsaha::create(['nama' => $request->nama]);
+            return response()->json([
+                'success' => true,
+                'message' => 'Bidang Usaha berhasil di tambahkan'
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalah create Bidang Usaha',
+                'error' => $th
+            ], 500);
+        }
+    }
 }
